58901f052740821cadb800b417a6f55ee2805cf4
Utkarsh <rajpututkarsh530@gmail.com>
2021-02-07 02:47:23 +0530

    Add deps

--- src/SciMLNLSolve.jl
+++ src/SciMLNLSolve.jl
@@ -3,10 +3,11 @@ module SciMLNLSolve
 using Reexport
 using NLsolve
 using MINPACK
+using LineSearches
 
 @reexport using SciMLBase
 include("algorithms.jl")
 include("solve.jl")
 
-export JuliaNLsolve, CMINPACK
+export NLSolveJL, CMINPACK
 end # module
