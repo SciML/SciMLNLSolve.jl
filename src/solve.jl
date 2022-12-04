e308b8bb4cd5ee4be22104331262514d0c3c30c9
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-12-04 11:30:43 +0100

    format

--- src/solve.jl
+++ src/solve.jl
@@ -67,23 +67,24 @@ function SciMLBase.solve(prob::Union{SciMLBase.AbstractSteadyStateProblem{uType,
     resid = similar(u0)
 
     original = nlsolve(f!, u0,
-                     xtol = reltol,
-                     ftol = abstol,
-                     iterations = maxiters,
-                     method = method,
-                     autodiff = autodiff,
-                     store_trace = store_trace,
-                     extended_trace = extended_trace,
-                     linesearch = linesearch,
-                     linsolve = linsolve,
-                     factor = factor,
-                     autoscale = autoscale,
-                     m = m,
-                     beta = beta,
-                     show_trace = show_trace)
+                       xtol = reltol,
+                       ftol = abstol,
+                       iterations = maxiters,
+                       method = method,
+                       autodiff = autodiff,
+                       store_trace = store_trace,
+                       extended_trace = extended_trace,
+                       linesearch = linesearch,
+                       linsolve = linsolve,
+                       factor = factor,
+                       autoscale = autoscale,
+                       m = m,
+                       beta = beta,
+                       show_trace = show_trace)
     u = reshape(original.zero, size(u))
     f!(resid, u)
-    retcode = original.x_converged || original.f_converged ? ReturnCode.Success : ReturnCode.Failure
+    retcode = original.x_converged || original.f_converged ? ReturnCode.Success :
+              ReturnCode.Failure
     SciMLBase.build_solution(prob, alg, u, resid; retcode = retcode,
-    original = original)
+                             original = original)
 end
