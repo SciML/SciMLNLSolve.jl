3083611e7c347de5755c9946449e4f014889f63e
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-01-17 13:23:08 -0500

    format

--- src/solve.jl
+++ src/solve.jl
@@ -1,14 +1,15 @@
 function SciMLBase.__solve(prob::Union{SciMLBase.AbstractSteadyStateProblem,
-                                     SciMLBase.AbstractNonlinearProblem},
-                         alg::algType,
-                         reltol = 1e-3,
-                         abstol = 1e-6,
-                         maxiters = 100000,
-                         timeseries = [],
-                         ts = [],
-                         ks = [],
-                         recompile::Type{Val{recompile_flag}} = Val{true};
-                         kwargs...) where {algType <: SciMLNLSolveAlgorithm, recompile_flag}
+                                       SciMLBase.AbstractNonlinearProblem},
+                           alg::algType,
+                           reltol = 1e-3,
+                           abstol = 1e-6,
+                           maxiters = 100000,
+                           timeseries = [],
+                           ts = [],
+                           ks = [],
+                           recompile::Type{Val{recompile_flag}} = Val{true};
+                           kwargs...) where {algType <: SciMLNLSolveAlgorithm,
+                                             recompile_flag}
     if typeof(prob.u0) <: Number
         u0 = [prob.u0]
     else
