7cb2da09940ac78b641c746b05d0c722a2d347ce
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-05-05 18:23:34 -0400

    Format

--- test/autodiff.jl
+++ test/autodiff.jl
@@ -12,3 +12,32 @@ sol = solve(prob, NLSolveJL(autodiff = :forward))
 du = zeros(2)
 f!(du, sol.u, nothing)
 @test maximum(du) < 1e-6
+
+function problem(x, A)
+    return x .^ 2 - A
+end
+
+function problemJacobian(x, A)
+    return diagm(2 .* x)
+end
+
+function f!(F, u, p)
+    F[1:152] = problem(u, p)
+end
+
+function j!(J, u, p)
+    J[1:152, 1:152] = problemJacobian(u, p)
+end
+
+f = NonlinearFunction(f!)
+
+init = ones(152);
+A = ones(152);
+A[6] = 0.8
+
+f = NonlinearFunction(f!, jac = j!)
+
+p = A
+
+ProbN = NonlinearProblem(f, init, p)
+sol = solve(ProbN, NLSolveJL(), reltol = 1e-8, abstol = 1e-8)
